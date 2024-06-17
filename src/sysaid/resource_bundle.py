d7483bb97c15ab1d5bf8561c2341691347eb03e7
Zeheng Li <imzehengl@gmail.com>
2024-06-17 15:22:54 -0600

    add rb

--- src/sysaid/resource_bundle.py
+++ src/sysaid/resource_bundle.py
@@ -1,2 +1,15 @@
+from urllib.parse import urljoin
+
+from requests.models import PreparedRequest
+
+from .core import process_response
+
+
 class ResourceBundleMixin:
-    pass
+    @process_response
+    def get_rb_translated_keys(self, **kwargs):
+        url = urljoin(self.host, "rb")
+        req = PreparedRequest()
+        req.prepare_url(url, kwargs)
+        url = req.url
+        return self.make_request("GET", url)
